c75353e22e0b033702b1a0166efedb1fd8ae67eb
arlanstore <122625984+arlanstore@users.noreply.github.com>
2023-04-15 08:52:06 +0700

    Add files via upload

---
+++ menu/auto-reboot.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+CEKEXPIRED () {
+    today=$(date -d +1day +%Y-%m-%d)
+    Exp1=$(curl -sS https://raw.githubusercontent.com/arlanstore/Profile/main/Profile/permission/ip | grep $MYIP | awk '{print $3}')
+    if [[ $today < $Exp1 ]]; then
+    echo -e "\e[32mACTIVE SCRIPT STATUS.....\e[0m"
+    else
+    echo -e "\e[31mYOUR SCRIPT EXPIRED!\e[0m";
+    echo -e "\e[31mRenew your IP Chat me okay? WhatsApp: +2348084124966 #\e[0m"
+    exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/arlanstore/Profile/main/Profile/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+echo -e "\e[31mRenew your IP Chat me okay? WhatsApp: +2348084124966 #, If you have registered, you just have to enjoy the results, hehe...... #\e[0m"
+exit 0
+fi
+clear
+if [ ! -e /usr/local/bin/reboot_otomatis ]; then
+echo '#!/bin/bash' > /usr/local/bin/reboot_otomatis
+echo 'tanggal=$(date +"%m-%d-%Y")' >> /usr/local/bin/reboot_otomatis
+echo 'waktu=$(date +"%T")' >> /usr/local/bin/reboot_otomatis
+echo 'echo "Server successfully rebooted on the date of $tanggal hit $waktu." >> /root/log-reboot.txt' >> /usr/local/bin/reboot_otomatis
+echo '/sbin/shutdown -r now' >> /usr/local/bin/reboot_otomatis
+chmod +x /usr/local/bin/reboot_otomatis
+fi
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"*
+echo -e "\e[0;100;33m       • AUTO-REBOOT MENU BY ARLAN STORE •          \e[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "[\e[36m•1\e[0m] Set Auto-Reboot Every 1 Hour"
+echo -e "[\e[36m•2\e[0m] Set Auto-Reboot Every 6 Hour"
+echo -e "[\e[36m•3\e[0m] Set Auto-Reboot Every 12 Hours"
+echo -e "[\e[36m•4\e[0m] Set Auto-Reboot Every 1 Day"
+echo -e "[\e[36m•5\e[0m] Set Auto-Reboot Every 1 Week"
+echo -e "[\e[36m•6\e[0m] Set Auto-Reboot Every 1 Month"
+echo -e "[\e[36m•7\e[0m] Turn off Auto-Reboot"
+echo -e "[\e[36m•8\e[0m] View reboot log"
+echo -e "[\e[36m•9\e[0m] Remove reboot log"
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e ""
+echo -e "Press x or [ Ctrl+C ] • To-Exit"
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " x
+if test $x -eq 1; then
+echo "10 * * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been set every an hour."
+elif test $x -eq 2; then
+echo "10 */6 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set every 6 hours."
+elif test $x -eq 3; then
+echo "10 */12 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set every 12 hours."
+elif test $x -eq 4; then
+echo "10 0 * * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a day."
+elif test $x -eq 5; then
+echo "10 0 */7 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a week."
+elif test $x -eq 6; then
+echo "10 0 1 * * root /usr/local/bin/reboot_otomatis" > /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot has been successfully set once a month."
+elif test $x -eq 7; then
+rm -f /etc/cron.d/reboot_otomatis
+echo "Auto-Reboot successfully TURNED OFF."
+elif test $x -eq 8; then
+if [ ! -e /root/log-reboot.txt ]; then
+	clear
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\e[0;100;33m        • AUTO-REBOOT LOG BY ARLANSTORE •        \e[0m"
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e ""
+    echo "No reboot activity found"
+    echo -e ""
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    read -n 1 -s -r -p "Press any key to back on menu"
+    auto-reboot
+	else
+	clear
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e "\e[0;100;33m        • AUTO-REBOOT LOG BY ARLANSTORE •        \e[0m"
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e ""
+	echo 'LOG REBOOT'
+	cat /root/log-reboot.txt
+    echo -e ""
+    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    read -n 1 -s -r -p "Press any key to back on menu"
+    auto-reboot
+fi
+elif test $x -eq 9; then
+clear
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[0;100;33m        • AUTO-REBOOT LOG BY ARLAN STORE •          \e[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo "" > /root/log-reboot.txt
+echo "Auto Reboot Log successfully deleted!"
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+auto-reboot
+elif test $x -eq 0; then
+clear
+m-system
+else
+clear
+echo ""
+echo "Options Not Found In Menu"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+auto-reboot
+fi
